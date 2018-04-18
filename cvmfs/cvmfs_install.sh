8cf2b36a20e9104aaa502a4d8e6b38fcc19c0c7b
Mark Ashdown <maja1@mrao.cam.ac.uk>
2018-04-18 12:10:10 +0100

    Configure for softdrive.nl to get LOFAR software

--- cvmfs/cvmfs_install.sh
+++ cvmfs/cvmfs_install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# Install packages.
+
 yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm
 yum install -y cvmfs cvmfs-config-default
+
+# Run setup script.
+
 cvmfs_config setup
-cp /vagrant/default.local /etc/cvmfs/default.local
+
+# Copy configuration files.
+
+cp /vagrant/etc-cvmfs/default.local /etc/cvmfs
+cp /vagrant/etc-cvmfs/config.d/softdrive.nl.conf /etc/cvmfs/config.d
+cp /vagrant/etc-cvmfs/keys/softdrive.nl.pub /etc/cvmfs/keys
