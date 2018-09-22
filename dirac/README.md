be58829422804c94245cde6329a2d397d2b84558
Mark Ashdown <maja1@mrao.cam.ac.uk>
2018-09-22 18:03:25 +0100

    Update DIRAC version and fix dependencies

--- dirac/README.md
+++ dirac/README.md
@@ -1,3 +1,8 @@
 # DIRAC
 
 Vagrantfile to set up Centos 7 virtual machine with DIRAC software.
+
+It assumes you have converted your Grid certificate into the
+appropriate .pem files and put them into ~/.globus on the host
+machine. That directory is copied into the virtual machine in the
+vagrant user's home directory.
--- dirac/dirac_install.sh
+++ dirac/dirac_install.sh
@@ -1,11 +1,12 @@
 dirac_dir=dirac
-dirac_version=v6r19p18
+dirac_version=v6r20p14
+lcg_version=v14r2
 
 mkdir -p $dirac_dir
 pushd $dirac_dir
 curl -o dirac-install https://raw.githubusercontent.com/DIRACGrid/DIRAC/integration/Core/scripts/dirac-install.py
 chmod +x dirac-install
-./dirac-install -r $dirac_version
+./dirac-install -r $dirac_version -g $lcg_version
 rm -f dirac-install
 popd
 
